06f7611af2402fda76a54ad48a11a593b7bb539a
Satwika <satwika@satwikas-mbp.mynetworksettings.com>
2024-10-11 23:11:55 -0400

    Updated Recipe.js to fix name issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Recipe App",
-  description: "Satwika's Recipe App",
+  description: "Satwika Recipe App",
 };
 
 export default function RootLayout({
